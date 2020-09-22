4f6f5f9dd0dfdc20d94caa71113efecbd081e891
ramrow17 <56510227+ramrow17@users.noreply.github.com>
2020-09-22 14:46:30 -0500

    first commit

---
+++ MenuProto.cpp
@@ -0,0 +1,17 @@
+// Here is the beginning of the menu logic code
+// Last edit: September 22, 2020
+
+#include <iostream> 
+#include <stdlib.h> 
+
+using namespace std; 
+
+int main () 
+{
+
+
+
+
+    return 0; 
+
+}
