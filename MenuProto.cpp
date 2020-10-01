c30650a36fd1157325f7f0330f85d45150e7e4ab
ramrow17 <56510227+ramrow17@users.noreply.github.com>
2020-09-30 21:49:41 -0500

    Finished submenu 1 - done with functions and tests

--- MenuProto.cpp
+++ MenuProto.cpp
@@ -3,6 +3,8 @@
 // Sept22 -> got a basic menu working and tested
 // Sept23 -> added sub menus and setup for more functionality
 // Sept27 -> Worked in sub menu 1 functionality 
+// Sept29 -> Tested possibilities for confirmation in submenu 1
+// Sept30 -> Finished up functionality with submenu 1 and testing
 
 
 #ifdef _WIN32
@@ -19,7 +21,7 @@ using namespace std;
 
 typedef void (*Menu_Processing_Function_Pointer)(void);
 
-static const char* ColorChoices[] = {"Blue", "Orange", "Green", "Red", "Pink", "Yellow", "Purple", }; 
+static const char* ColorChoices[] = {"Blue", "Orange", "Green", "Red", "Pink", "Yellow", "Purple", "Black"}; 
 
 struct Menu_Option
 {
@@ -35,7 +37,7 @@ unsigned int PlayerCount = 0;
 
 // Main function that will use whatever title and submenu necessary
 void MenuFunc(const char*, const Menu_Option*); 
-
+void ClearScreen(unsigned int); 
 
 
 // The following functions, menu declaration and functions will be later moved to
@@ -49,10 +51,10 @@ void Process_MMSelection4(); // reset all values for things
 
 static const Menu_Option main_menu[] =
 {
-  {'1', "Option 1", Process_MMSelection1},
-  {'2', "Option 2", Process_MMSelection2},
-  {'3', "Option 3", Process_MMSelection3},
-  {'4', "Option 4", Process_MMSelection4}
+  {'1', "1 - SubMenu 1", Process_MMSelection1},
+  {'2', "2 - SubMenu 2", Process_MMSelection2},
+  {'3', "3 - SubMenu 3", Process_MMSelection3},
+  {'4', "4 - SubMenu 4 or is it..?", Process_MMSelection4}
 };
 // End of declaration for Main menu schtuff
 
@@ -62,18 +64,21 @@ static const Menu_Option main_menu[] =
 // Sub Menu 1 Selection
 // Will be used for the Player amount selection (2-6) 
 void Process_S1Selection1(); // Add
-void Process_S1Selection2(); // Confirm
-void Process_S1Selection3(); // Subtract
-void Process_S1Selection4(); // Reset to 0
+void Process_S1Selection2(); // Subtract
+void Process_S1Selection3(); // Reset PlayerCount
+void Process_S1Selection4(); // Confirm
 
 static const Menu_Option sub1_menu[] =
 {
-  {'1', "Option 1", Process_S1Selection1},
-  {'2', "Option 2", Process_S1Selection2},
-  {'3', "Option 3", Process_S1Selection3},
-  {'4', "Option 4", Process_S1Selection4}
+  {'1', "1 - Add", Process_S1Selection1},
+  {'2', "2 - Subtract", Process_S1Selection2},
+  {'3', "3 - Reset PlayerCount", Process_S1Selection3},
+  {'4', "4 - Custom Amount", Process_S1Selection4}
 };
 // End of declaration for Sub menu 1
+
+
+
 
 // Sub Menu 2 Selection
 // Will be used for the Player color selection
@@ -128,7 +133,7 @@ int main()
     "         Beginning Menu\n"
     "------------------------------\n"
     ;
-
+	PlayerCount = 2; 
     MenuFunc(menu_title, main_menu); 
 
   return EXIT_SUCCESS;
@@ -138,14 +143,13 @@ int main()
 
 
 
-
-
+// *** 	MAIN MENU SELECTION START HERE 	*** 
 
 
 void Process_MMSelection1()
 { 
   
-  system("cls"); 
+  ClearScreen(0);  
   static const char* SubMenuTitle = 
   "------------------------------\n"
   "         First Sub Menu\n"
@@ -153,14 +157,13 @@ void Process_MMSelection1()
 
 
   cout << "Lets go to submenu 1!\n"; 
-  Sleep(3000);
-  system("cls");
+  ClearScreen(3); 
 
   MenuFunc(SubMenuTitle, sub1_menu); 
 
   // Exiting the function...
   cout << "Goodbye - now exiting sub menu 1"; 
-  Sleep(3000); system("cls"); return; 
+  ClearScreen(3);  return; 
   
 } // ****  end Process_MMSelection1()  ****
 
@@ -170,7 +173,7 @@ void Process_MMSelection1()
 void Process_MMSelection2()
 { 
 
-  system("cls"); 
+  ClearScreen(0);  
   static const char* SubMenuTitle = 
   "------------------------------\n"
   "         Second Sub Menu\n"
@@ -178,14 +181,13 @@ void Process_MMSelection2()
 
 
   cout << "Lets go to submenu 2!\n"; 
-  Sleep(3000);
-  system("cls");
+  ClearScreen(3); 
 
   MenuFunc(SubMenuTitle, sub2_menu); 
 
   // Exiting the function...
   cout << "Goodbye - now exiting sub menu 2"; 
-  Sleep(3000); system("cls"); return; 
+  ClearScreen(3);  return; 
   
 } // ****  end Process_MMSelection2()  ****
 
@@ -203,14 +205,13 @@ void Process_MMSelection3()
 
 
   cout << "Lets go to submenu 3!\n"; 
-  Sleep(3000);
-  system("cls");
+  ClearScreen(3); 
 
   MenuFunc(SubMenuTitle, sub3_menu); 
 
   // Exiting the function...
   cout << "Goodbye - now exiting sub menu 3"; 
-  Sleep(3000); system("cls"); return; 
+  ClearScreen(3);  return; 
   
 } // ****  end Process_MMSelection3()  ****
 
@@ -218,38 +219,66 @@ void Process_MMSelection3()
 
 void Process_MMSelection4() 
 { 
-  system("cls");
+  ClearScreen(1); 
   cout << "So we're not going into a sub menu or anything but... Hey :-)\n"; 
-  Sleep(1000);
-
+  Sleep(1000); 
   // Exiting the function...
   cout << "Goodbye - now exiting option 4"; 
-  Sleep(1000); system("cls"); return; 
+  ClearScreen(1);  return; 
 
 } // ****  end Process_MMSelection4()  ****
 
 
-void Process_S1Selection1() { 
-  PlayerCount++;
-  cout << "Updated Player count:" << PlayerCount << endl;
-  Sleep(1000); system("cls");
+
+
+
+
+
+
+
+// ********* BEGIN PLAYER COUNT SUB MENU *********
+
+
+// *** 		ADDING 1 TO PLAYER COUNT 	****
+void Process_S1Selection1() {
+  if (PlayerCount < 6) PlayerCount++;
+  cout << "Updated player count: " << PlayerCount << endl;
+  ClearScreen(1); 
   return;
 }
 
 
+// **** 	REDUCING PLAYERCOUNT BY 1 	****
 void Process_S1Selection2() { 
-  
-  return; 
+  if (PlayerCount > 2) PlayerCount--; 
+  cout << "Updated player count: " << PlayerCount << endl; 
+  ClearScreen(1); 
+  return;
 }
 
+
+// **** 	RESETTING PLAYERCOUNT   	****
 void Process_S1Selection3() { 
-  if (PlayerCount != 0) PlayerCount--; 
-  cout << "Updated Player count:" << PlayerCount << endl; 
-  Sleep(1000); system("cls");
-  return;
+	PlayerCount = 2; cout << "Updated player count: " << PlayerCount << endl; 
+  ClearScreen(1);  return; 
    }
 
-void Process_S1Selection4() { PlayerCount = 0; return; }
+// ****		CUSTOM PLAYERCOUNT VAL		****
+void Process_S1Selection4() { 
+
+	unsigned int TempVal = 0;
+	
+	// *** Need to test this while loop, the not symbols might be wrong idk
+	while(((TempVal > 6) || (TempVal < 2)))
+	{
+		cout << "Enter any number 2-6:";
+		cin >> TempVal;	
+	}
+	
+	cout << "Updated player count: " << TempVal << endl; 
+	PlayerCount = TempVal; ClearScreen(1); 
+	return; 
+}
 
 
 
@@ -285,7 +314,13 @@ void MenuFunc(const char* title, const Menu_Option* MenuChoice)
     {
         cout << MenuChoice[i].p_selection_text << "\n";
     }
-    cout << "Enter selection, 0 to quit: ";
+	
+	
+	// *** TEST HERE TO MAKE SURE TERNARY OPERATOR IS WORKING PROPERLY *** 
+    cout << "Enter selection, "
+		<< ((MenuChoice == main_menu) ?  "enter 0 to quit" : "enter 0 to confirm selection(s)") 
+		<< endl; 
+		
     cin >> choice;
     if (choice == '0')  { flag = true; break; } 
     for (size_t i = 0; i < quantity_selections; ++i)
@@ -303,3 +338,11 @@ void MenuFunc(const char* title, const Menu_Option* MenuChoice)
 
 }
 
+
+
+void ClearScreen (unsigned int seggs)
+{
+  Sleep(seggs*1000); 
+  system("cls"); // "cls" is used for window - Linux probably would just use "clear"
+  return; 
+}
